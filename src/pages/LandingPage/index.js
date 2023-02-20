0907d5e58b8308ad1bf701652d61584068b8b892
Rut Martinez <rut.martinez@mercadolibre.cl>
2023-02-20 15:54:17 -0300

    Update in meni

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -7,7 +7,7 @@ export default function LandingPage() {
            <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
                <div class="container">
                    <a class="navbar-brand" href="#page-top"><img src="img/navbar-logo.svg" alt="..." /></a>
-                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                        Menu
                        <i class="fas fa-bars ms-1"></i>
                    </button>
